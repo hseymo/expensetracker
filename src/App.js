5bff037d89a1048db49bcb0f066e9c9f61c76927
Haley Seymour <haleycseymour@comcast.net>
2022-07-25 13:52:10 -0700

    browserrouter start and home page folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'
 
 function App() {
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [userId, setUserId] = useState(null);
+  // const [token, setToken] = useState(null);
+
+  // // check local storage for token on page load
+  // useEffect(()=>{
+  //   const storedToken = localStorage.getItem("token")
+  //   if (storedToken) {
+  //     setToken(storedToken)
+  //   }
+  // }, [])
+
+  // // if there is a token, verify it using API route. set isloggedin to true and set user id. 
+  // useEffect(()=> {
+  //     if(token) {
+  //       API.verify(token)
+  //       .then(userData => {
+  //         if (userData.userId){
+  //           setIsLoggedIn(true);
+  //           setUserId(userData.userId)
+  //         } else {
+  //           setIsLoggedIn(false);
+  //           setUserId(null)
+  //         }
+  //       }).catch((err) => console.log(err))
+  //     } else {
+  //       setIsLoggedIn(false);
+  //       setUserId(null)
+  //     }
+  //   }, [token])
+
+  //   // on log in, set token in local storage 
+  //   const handleLoginSubmit = (loginData) => {
+  //     API.login(loginData)
+  //     .then(data => {
+  //       console.log(data)
+  //       if (data.token) {
+  //         setToken(data.token);
+  //         localStorage.setItem("token", data.token);
+  //         window.location.href = "/dashboard"
+  //       } else {
+  //         setErrorMessage('Login failed; please try again.')
+  //       }
+  //     })
+  //     .catch ((err) => console.log(err))
+  //   }
+  
+  //   // on sign up, set token in local storage
+  //   const handleSignupSubmit = (signupData) => {
+  //     API.signup(signupData)
+  //     .then(data => {
+  //       if (data.token) {
+  //         setToken(data.token)
+  //         localStorage.setItem("token", data.token);
+  //         window.location.href = "/dashboard"
+  //       } else {
+  //         setErrorMessage('Signup failed; please try again.')
+  //       }
+  //     })
+  //     .catch((err) => console.log(err))
+  //   }
+  
+  //   // logout by removing the token
+  //   const logout = () => {
+  //     setToken(null);
+  //     localStorage.removeItem('token');
+  //   }
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<BrowserRouter>
+  {/* <Header>
+    isLoggedIn={isLoggedIn}
+    userId={userId}
+    logout={logout}
+  </Header> */}
+  <Routes>
+    <Route
+      path='/'
+      element={<Home
+        // isLoggedIn={isLoggedIn}
+      />} 
+    />
+  </Routes>
+</BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import './index.css'
+
+function Home(
+  // {isLoggedIn}
+  ) {
+
+  return (
+    <div>
+      <header className="header">
+        Welcome to my expense tracking application!
+      </header>
+      <p className="description"> The goal of this app is to allow you to custom build your expense tracker. While there are many expense trackers out there, I aim to create one that is customizable and gives you the ability to track your expenses in any manner you desire.</p>
+      {/* {!isLoggedIn ? (
+        <button className='enter' onClick={(e) => {window.location.href = "/login"}}>Login</button>
+          ) : (
+        <button className='enter' onClick={(e) => {window.location.href = "/dashboard"}}>Dashboard</button>
+      )} */}
+    </div>
+  );
+}
+
+export default Home;
