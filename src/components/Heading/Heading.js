57bdb83070f2fc0fb364b61c795b18ed72e81533
Haley Seymour <haleycseymour@comcast.net>
2022-07-26 13:22:28 -0700

    styling of header and login page (basic)

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -5,8 +5,8 @@ import './Heading.css';
 export default function Heading ( {isLoggedIn, userId, logout } ) {
     return (
         <div className="Header">
-            <h1>Expense Tracker </h1>
-            < NavTabs isLoggedIn={isLoggedIn} userId={userId} logout={logout} />
+            <NavTabs isLoggedIn={isLoggedIn} userId={userId} logout={logout}/>
+            <h1>Expense Tracker</h1>
         </div>
     )
 }
